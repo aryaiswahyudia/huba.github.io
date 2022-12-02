c8c20a8dfea1891610fb1e74779e04b14ca1bb26
aryaiswahyudia <115598495+aryaiswahyudia@users.noreply.github.com>
2022-12-02 08:32:20 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<html>
+<head>
+<title>Login from</title>
+<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+<div class="login-form">
+ <h1>Login from</h1>
+ <form action="#" method="post">
+  <p>Username</p>
+  <input type="text" name="user" placeholder="Username">
+  <p>Password</p>
+  <input type="text" name="password" placeholder="Password">
+  <button type="submit">login</button>
+  </form>
+</div>
+</body>
+</htm>
